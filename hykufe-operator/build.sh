5fa3539ed23871c33754b54ff1a6492cf25e07c0
hyeongyun0916 <mhg5303@gmail.com>
2019-09-27 04:59:47 +0000

    set go module

--- hykufe-operator/build.sh
+++ hykufe-operator/build.sh
@@ -3,6 +3,8 @@ set -x
 [ "$#" -eq 1 ] || { echo 'version is required e.g. v0.0.1'; exit 1; }
 version=$1
 
+export GO111MODULE=on
+
 operator-sdk generate k8s
 operator-sdk generate openapi
 operator-sdk build yoowj7472/hykufe-operator:${version}
