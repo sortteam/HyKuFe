e97d28c745948bdd9cd90bac9864f42163cd094b
hyeongyun0916 <mhg5303@gmail.com>
2019-11-08 02:02:59 +0900

    #16 change Test code function name

--- hykufe-client/main.py
+++ hykufe-client/main.py
@@ -2,6 +2,8 @@ import hykufe
 
 hykufe.HyKuFeBuilder()\
     .setName("test1").setImage("test2")\
-    .setCpu("test3").setMemory("test4")\
-    .setGpu("test5").setReplica("test6")\
+    .setCPU("test3").setMemory("test4")\
+    .setGPU("test5").setReplica("test6")\
     .build().writeYamlFile("test.yaml")
+
+# hykufe.HyKuFeBuilder().build().createJOB()
