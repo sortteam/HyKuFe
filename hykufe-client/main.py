e6873d31415744c80dbb8a042640b2bdaea39bc5
hyeongyun0916 <mhg5303@gmail.com>
2019-11-08 02:47:12 +0900

    #20 upload file to s3

--- hykufe-client/main.py
+++ hykufe-client/main.py
@@ -1,9 +1,10 @@
 import hykufe
 
-hykufe.HyKuFeBuilder()\
-    .setName("test1").setImage("test2")\
-    .setCPU("test3").setMemory("test4")\
-    .setGPU("test5").setReplica("test6")\
-    .build().writeYamlFile("test.yaml")
+# hykufe.HyKuFeBuilder()\
+#     .setName("test1").setImage("test2")\
+#     .setCPU("test3").setMemory("test4")\
+#     .setGPU("test5").setReplica("test6")\
+#     .build().writeYamlFile("test.yaml")
 
-# hykufe.HyKuFeBuilder().build().createJOB()
+hykufe.HyKuFeBuilder()..build('access_key', 'secret_key').uploadFileToS3('main.py')
+
